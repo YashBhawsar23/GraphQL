4f1c4cbc02fefbd80618af7f53d329fe103c5f6e
Yash Bhawsar <yashbhawsar97@gmail.com>
2025-02-16 23:19:52 +0530

    env

--- graphql-server/index.js
+++ graphql-server/index.js
@@ -1,6 +1,16 @@
+require("dotenv").config();
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("Connection Error", err));
 
 const app = express();
 
